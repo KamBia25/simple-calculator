8fcba50d945b3d80119b2d73faca68f04321f870
Kamil <kamilbialek2k05@gmail.com>
2023-03-24 16:06:57 +0100

    l

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simple-calculator
+This project consist of basic mathematical operations using javascript with html and css.
+First thing to do is to make functions of those mathematical operations.
---
+++ scripts.js
@@ -0,0 +1,13 @@
+function add(firstVariable,secondVariable){
+    operator = `+`
+    return firstVariable + secondVariable;
+}
+function subtract(minuend,subtrahend){
+    return minuend - subtrahend;
+}
+function multiply(firstVariable,secondVariable){
+    return firstVariable * secondVariable;
+}
+function divide(dividend,divisor){
+    return dividend/divisor;
+}

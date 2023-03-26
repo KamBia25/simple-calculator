c3c074a43d5130398549dbafe6aa041cac9ef617
Kamil <kamilbialek2k05@gmail.com>
2023-03-26 10:06:01 +0200

    funtion operate for calculations

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-function add(firstVariable,operator,secondVariable){
+function add(firstVariable, operator, secondVariable){
     return `${firstVariable} ${operator} ${secondVariable}`;
 }  
-function subtract(firstVariable,operator,secondVariable){
+function subtract(firstVariable, operator, secondVariable){
     return `${firstVariable} ${operator} ${secondVariable}`;
 }
-function multiply(firstVariable,operator,secondVariable){
+function multiply(firstVariable, operator, secondVariable){
     return `${firstVariable} ${operator} ${secondVariable}`;
 }
-function divide(firstVariable,operator,secondVariable){
+function divide(firstVariable, operator, secondVariable){
     return `${firstVariable} ${operator} ${secondVariable}`;
+}
+function operate(firstVariable,operator,secondVariable){
+    switch(operator) {
+        case "+":
+          return `${firstVariable} ${operator} ${secondVariable}`;
+        case "-":
+          return `${firstVariable} ${operator} ${secondVariable}`;
+        case "*":
+          return `${firstVariable} ${operator} ${secondVariable}`;
+        case "/":
+          return `${firstVariable} ${operator} ${secondVariable}`;
 }

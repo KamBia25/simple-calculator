9e33b2e4e6ecf020d7127e20f774c5216cd908f1
Kamil <kamilbialek2k05@gmail.com>
2023-03-31 14:50:10 +0200

    operational buttons stores value form input to variables

--- scripts.js
+++ scripts.js
@@ -22,10 +22,33 @@ function operate(firstVariable,operator,secondVariable){
           divide(firstVariable, operator, secondVariable);
 }
 }
+function clearInput(){
+  var getValue= document.getElementById("input");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }
+}
+let firstVariable = "";
+let secondVariable = "";
+let operator = "";
+
 let numeralButtons = document.getElementsByClassName("numeral");
 let input = document.getElementById("input");
 for (let i = 0 ; i < numeralButtons.length; i++) {
   numeralButtons[i].addEventListener('click', function() {
     input.value = input.value + this.textContent ;
+  });
+}
+let operationalButtons = document.getElementsByClassName("operational");
+for (let i = 0 ; i <operationalButtons.length; i++) {
+  operationalButtons[i].addEventListener('click', function() {
+    if(firstVariable!=""){
+    secondVariable = input.value;
+    clearInput()
+    }
+    else{
+      firstVariable = input.value;
+      clearInput()
+    }
   });
 }

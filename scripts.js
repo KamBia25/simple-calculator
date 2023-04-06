813f57ba5de608d82e8106a023b8a997632c3560
Kamil <kamilbialek2k05@gmail.com>
2023-04-06 11:10:44 +0200

    change mathematical opeations functions and remade operationalButtons function so it works

--- scripts.js
+++ scripts.js
@@ -1,54 +1,64 @@
-function add(firstVariable, operator, secondVariable){
-    return `${firstVariable} ${operator} ${secondVariable}`;
-}  
-function subtract(firstVariable, operator, secondVariable){
-    return `${firstVariable} ${operator} ${secondVariable}`;
+function add(firstVariable, secondVariable){
+ return firstVariable + secondVariable;
 }
-function multiply(firstVariable, operator, secondVariable){
-    return `${firstVariable} ${operator} ${secondVariable}`;
+function subtract(firstVariable, secondVariable){
+    return firstVariable - secondVariable;
 }
-function divide(firstVariable, operator, secondVariable){
-    return `${firstVariable} ${operator} ${secondVariable}`;
+function multiply(firstVariable, secondVariable){
+    return firstVariable * secondVariable;
 }
-function operate(firstVariable,operator,secondVariable){
-    switch(operator) {
-        case "+":
-          add(firstVariable, operator, secondVariable);
-        case "-":
-          subtract(firstVariable, operator, secondVariable);
-        case "*":
-          multiply(firstVariable, operator, secondVariable);
-        case "/":
-          divide(firstVariable, operator, secondVariable);
+function divide(firstVariable, secondVariable){
+    return firstVariable / secondVariable;
 }
+function operate(firstVariable, operator, secondVariable) {
+  switch(operator) {
+    case "+":
+      return add(firstVariable, secondVariable);
+    case "-":
+      return subtract(firstVariable, secondVariable);
+    case "*":
+      return multiply(firstVariable, secondVariable);
+    case "/":
+      return divide(firstVariable, secondVariable);
+    default:
+      return "Invalid operator";
+  }
 }
+
 function clearInput(){
   var getValue= document.getElementById("input");
     if (getValue.value !="") {
         getValue.value = "";
     }
 }
-let firstVariable = "";
-let secondVariable = "";
-let operator = "";
 
-let numeralButtons = document.getElementsByClassName("numeral");
+const numeralButtons = document.getElementsByClassName("numeral");
 let input = document.getElementById("input");
 for (let i = 0 ; i < numeralButtons.length; i++) {
   numeralButtons[i].addEventListener('click', function() {
     input.value = input.value + this.textContent ;
   });
 }
-let operationalButtons = document.getElementsByClassName("operational");
-for (let i = 0 ; i <operationalButtons.length; i++) {
-  operationalButtons[i].addEventListener('click', function() {
-    if(firstVariable!=""){
-    secondVariable = input.value;
+let firstVariable="";
+let secondVariable="";
+let operator="";
+const operationalButtons = document.getElementsByClassName("operational");
+for(let i = 0; i < operationalButtons.length; i++){
+  operationalButtons[i].addEventListener('click',function(event){
+    const button = event.target;
+    if(firstVariable==""){
+    firstVariable=parseInt(input.value);
+    operator=button.value;
     clearInput()
     }
     else{
-      firstVariable = input.value;
-      clearInput()
+    secondVariable=parseInt(input.value);
+    clearInput()
+    input.value=operate(firstVariable, operator, secondVariable) 
+    firstVariable="";
+    secondVariable="";
+    operator=""; 
     }
-  });
+    
+  })
 }

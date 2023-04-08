f0f87265269b3db22de29d4594d3b3cdb1d0eddb
Kamil <kamilbialek2k05@gmail.com>
2023-04-08 13:28:30 +0200

    fix bug of  buttons

--- scripts.js
+++ scripts.js
@@ -37,28 +37,56 @@ let input = document.getElementById("input");
 for (let i = 0 ; i < numeralButtons.length; i++) {
   numeralButtons[i].addEventListener('click', function() {
     input.value = input.value + this.textContent ;
+    if(condition==2){
+      clearInput()
+      condition-=1;
+      input.value = input.value + this.textContent ;
+    }
   });
 }
 let firstVariable="";
 let secondVariable="";
 let operator="";
+let condition=0;
 const operationalButtons = document.getElementsByClassName("operational");
 for(let i = 0; i < operationalButtons.length; i++){
   operationalButtons[i].addEventListener('click',function(event){
     const button = event.target;
-    if(firstVariable==""){
+    if(firstVariable==""  && condition==0){
     firstVariable=parseInt(input.value);
     operator=button.value;
     clearInput()
+    condition+=1;
     }
-    else{
-    secondVariable=parseInt(input.value);
-    clearInput()
-    input.value=operate(firstVariable, operator, secondVariable) 
-    firstVariable="";
-    secondVariable="";
-    operator=""; 
+    else if(condition==1){
+      secondVariable=parseInt(input.value);
+      clearInput()
+      input.value=operate(firstVariable, operator, secondVariable)
+      firstVariable=parseInt(input.value); 
+      secondVariable="";
+      condition+=1;
+      operator=button.value;
     }
+    // else{
+    //   for (let i = 0 ; i < numeralButtons.length; i++) {
+    //     numeralButtons[i].addEventListener('click', function() {
+    //       if (condition==2){
+    //         clearInput()
+    //         condition-=1;
+    //       }
+    //     });
+    //   }
+
+    
+    // }
     
   })
 }
+const equalsButton = document.getElementById("button-=");
+equalsButton.addEventListener('click',function(){
+  if(firstVariable!=""  && operator!=""){
+    secondVariable=parseInt(input.value);
+      clearInput()
+      input.value=operate(firstVariable, operator, secondVariable) 
+  }
+})

c6d70fc6693a66b9efcaaae6a21cdd4d60bc0e9c
Kamil <kamilbialek2k05@gmail.com>
2023-11-15 07:06:59 +0100

    fix keyboard inserts, fix dot button and add it's keyboard support, create delete button that work like backspace key from keyboard

--- scripts.js
+++ scripts.js
@@ -1,100 +1,85 @@
 function add(firstVariable, secondVariable){
- return firstVariable + secondVariable;
-}
-function subtract(firstVariable, secondVariable){
-    return firstVariable - secondVariable;
-}
-function multiply(firstVariable, secondVariable){
-    return firstVariable * secondVariable;
-}
-function divide(firstVariable, secondVariable){
-    return firstVariable / secondVariable;
-}
-function operate(firstVariable, operator, secondVariable) {
-  switch(operator) {
-    case "+":
-      return add(firstVariable, secondVariable);
-    case "-":
-      return subtract(firstVariable, secondVariable);
-    case "*":
-      return multiply(firstVariable, secondVariable);
-    case "/":
-      return divide(firstVariable, secondVariable);
-    default:
-      return "Invalid operator";
-  }
-}
-let firstVariable="";
-let secondVariable="";
-let operator="";
-let condition=0;
+  return firstVariable + secondVariable;
+ }
+ function subtract(firstVariable, secondVariable){
+     return firstVariable - secondVariable;
+ }
+ function multiply(firstVariable, secondVariable){
+     return firstVariable * secondVariable;
+ }
+ function divide(firstVariable, secondVariable){
+     return firstVariable / secondVariable;
+ }
+ function operate(firstVariable, operator, secondVariable) {
+   switch(operator) {
+     case "+":
+       return add(firstVariable, secondVariable);
+     case "-":
+       return subtract(firstVariable, secondVariable);
+     case "*":
+       return multiply(firstVariable, secondVariable);
+     case "/":
+       return divide(firstVariable, secondVariable);
+     default:
+       return "Invalid operator";
+   }
+ }
+ 
+ function clearInput(){
+   var getValue= document.getElementById("input");
+     if (getValue.value !="") {
+         getValue.value = "";
+     }
+ }
+ function checkNumber(event){
+  let ASCInumber = event.which ? event.which :  event.keyCode;
+  console.log(event.keyCode)
+  if (ASCInumber > 31 && (ASCInumber < 48 || ASCInumber > 57 || ASCInumber==46)) return false;
+  return true;
+ }
+ 
+ const numeralButtons = document.getElementsByClassName("numeral");
+ let input = document.getElementById("input");
+ for (let i = 0 ; i < numeralButtons.length; i++) {
+   numeralButtons[i].addEventListener('click', function() {
+     input.value = input.value + this.textContent;
+ 
+       const dotButton = document.getElementById("button-.")
+       dotButton.addEventListener('click', function(){
+        if (input.value.includes(".")){
+          dotButton.removeEventListener('click',true)
+         }
+         else
+         input.value = input.value + this.textContent;
+         
+         
+       },{once : true})
+      
+     if(condition==4){
+       clearInput()
+       condition=0;
+       input.value = input.value + this.textContent;
+     }
+     if(condition==3){
+       clearInput()
+       condition-=1;
+       input.value = input.value + this.textContent;
+     }
+   });
+ }
+ let firstVariable="";
+ let secondVariable="";
+ let operator="";
+ let condition=0;
 
-function clearInput(){
-  var getValue= document.getElementById("input");
-    if (getValue.value !="") {
-        getValue.value = "";
-    }
-}
-function numberButton(event){
-  
-  input.value = input.value + event.target.textContent;
-  let value= input.value;
-  const dotButton = document.getElementById("button-.")
-  dotButton.addEventListener('click', function(){
-    input.value = input.value + this.textContent;
-    dotButton.removeEventListener('click')
-  })
-if(condition==4){
-  clearInput()
-  condition=0;
-  input.value = input.value + event.target.textContent;
 
-}
-if(condition==3){
-  clearInput()
-  condition-=1;
-  input.value = input.value + event.target.textContent;
 
-}
-updateInputValue(value)
-}
-function updateInputValue(value) {
-  if (value.length > maxLength) {
-    value = value.substring(0, maxLength);
-  }
-  input.value = value;
-}
-const numeralButtons = document.getElementsByClassName("numeral");
-let input = document.getElementById("input");
-const maxLength = input.getAttribute("maxLength");
-for (let i = 0 ; i < numeralButtons.length; i++) {
-  numeralButtons[i].addEventListener('click', function(event){
-    numberButton(event);
-  });
-  // numeralButtons[i].addEventListener('click', function() {
-  //   input.value = input.value + this.textContent;
 
-  //     const dotButton = document.getElementById("button-.")
-  //     dotButton.addEventListener('click', function(){
-  //       input.value = input.value + this.textContent;
-  //       dotButton.removeEventListener('click')
-  //     })
-  //   if(condition==4){
-  //     clearInput()
-  //     condition=0;
-  //     input.value = input.value + this.textContent;
-  //   }
-  //   if(condition==3){
-  //     clearInput()
-  //     condition-=1;
-  //     input.value = input.value + this.textContent;
-  //   }
-  // });
-}
+ const operationalButtons = document.getElementsByClassName("operational");
 
-const operationalButtons = document.getElementsByClassName("operational");
-for(let i = 0; i < operationalButtons.length; i++){
-  operationalButtons[i].addEventListener('click',function(event){
+ for(let i = 0; i < operationalButtons.length; i++){
+   operationalButtons[i].addEventListener('click',function(event)
+   {
     const button = event.target;
     if(firstVariable==""  && condition==0){
     firstVariable=parseFloat(input.value);
@@ -110,6 +95,7 @@ for(let i = 0; i < operationalButtons.length; i++){
     else if(condition==2){
       secondVariable=parseFloat(input.value);
       clearInput()
+  
       if(operator=="/" && secondVariable==0){
         input.value="ERROR"
         firstVariable="";
@@ -124,37 +110,149 @@ for(let i = 0; i < operationalButtons.length; i++){
       condition+=1;
       operator=button.value;
     }}
-  })
-}
-const equalsButton = document.getElementById("button-=");
-equalsButton.addEventListener('click',function(){
-  if(firstVariable!=""  && operator!=""){
-    if(operator=="/" && secondVariable==0){
-      input.value="ERROR"
-      firstVariable="";
-      secondVariable="";
-      operator="";
-      condition=4;
-    }
-    else{
-    secondVariable=parseFloat(input.value);
-      clearInput()
-      input.value=operate(firstVariable, operator, secondVariable) 
-      firstVariable=parseFloat(input.value);
-      operator="";  
   }
-}
+  )}
+    
+    
+input.addEventListener("keydown", function(event){
+  
+  if(event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/')
+  {
+    console.log(event.key)
+    
+    
+    {
+      
+      if(firstVariable==""  && condition==0){
+      firstVariable=parseFloat(input.value);
+     
+      operator=event.key;
+      console.log(operator)
+      clearInput()
+      condition+=2;
+      }
+      else if (firstVariable!="" && condition==2 && operator==""){
+        operator=event.key;
+        clearInput()
+        condition+=1;
+      }
+      else if(condition==2){
+        secondVariable=parseFloat(input.value);
+        clearInput()
+    
+        if(operator=="/" && secondVariable==0){
+          input.value="ERROR"
+          firstVariable="";
+          secondVariable="";
+          operator="";
+          condition=4;
+        }
+        else{
+        input.value=operate(firstVariable, operator, secondVariable)
+        firstVariable=parseFloat(input.value); 
+        secondVariable="";
+        condition+=1;
+        operator=event.key;
+      }}
+      
+    }
+      
+    
+       
+  }
 })
 
-const clear = document.getElementById("clearButton")
-clear.addEventListener('click',function(){
-  clearInput()
-  firstVariable="";
-  secondVariable="";
-  operator="";
-  condition=0;
-})
+input.addEventListener('keydown', function(event){
+  if(event.key==="."){
+    if (input.value.includes(".")){
+      input.removeEventListener('keydown',true)
+     }
+     else
+     input.value = input.value + event.key;
+     
+     
+   }})
+  
+ 
 
+
+
+
+ const equalsButton = document.getElementById("button-=");
+ equalsButton.addEventListener('click',function(){
+   if(firstVariable!=""  && operator!=""){
+    secondVariable=parseFloat(input.value); 
+     if(operator=="/" && secondVariable==0){
+       input.value="ERROR"
+       firstVariable="";
+       secondVariable="";
+       operator="";
+       condition=4;
+     }
+     else{
+     secondVariable=parseFloat(input.value);
+       clearInput()
+       input.value=operate(firstVariable, operator, secondVariable) 
+       firstVariable=parseFloat(input.value);
+       operator="";
+       secondVariable="";
+       firstVariable="";
+       condition=0;
+       
+   }
+ }
+ })
+
+ input.addEventListener("keydown", function(event){
+  if(event.key === '='){
+    if(firstVariable!=""  && operator!=""){
+      secondVariable=parseFloat(input.value); 
+       if(operator=="/" && secondVariable==0){
+         input.value="ERROR"
+         firstVariable="";
+         secondVariable="";
+         operator="";
+         condition=4;
+       }
+       else{
+       secondVariable=parseFloat(input.value);
+         clearInput()
+         input.value=operate(firstVariable, operator, secondVariable) 
+         firstVariable=parseFloat(input.value);
+         operator="";
+         secondVariable="";
+         firstVariable="";
+         condition=0;
+         
+     }
+   }
+  }
+ }
+ )
+ const clear = document.getElementById("clearButton")
+ clear.addEventListener('click',function(){
+   clearInput()
+   firstVariable="";
+   secondVariable="";
+   operator="";
+   condition=0;
+ })
+
+ const backspace = document.getElementById("backspaceButton")
+ backspace.addEventListener('click',function(){
+var numbersFromInput = input.value;
+ console.log(numbersFromInput);
+ let numberToArray = num => Number(num); 
+      
+    var arrayFromInput= Array.from(String(numbersFromInput), numberToArray);
+ console.log(arrayFromInput);
+ arrayFromInput.pop();
+ console.log(arrayFromInput)
+ numbersFromInput = arrayFromInput.join('');
+
+ console.log(numbersFromInput) 
+ input.value =  numbersFromInput;
+ });
 
 // const buttons = document.querySelectorAll('.my-button');
 //   buttons.forEach(button => {
@@ -172,3 +270,4 @@ clear.addEventListener('click',function(){
 //   window.addEventListener(event,mouseMoveHandler,false);
 //   numberButton(event)
 //  });
+
